eb24cfd6babf2feaf714ffbd0bb1f177bb293855
Max Ogden <max@maxogden.com>
2014-02-20 15:48:09 -0800

    rename geojson folder to test-data

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var fs = require('fs')
 var simplify = require('./')
 
 test('FeatureCollection w/ a LineString in it', function(t) {
-  var fcLine = JSON.parse(fs.readFileSync('./geojson/oakland-route.geojson'))
+  var fcLine = JSON.parse(fs.readFileSync('./test-data/oakland-route.geojson'))
   var len = fcLine.features[0].geometry.coordinates.length
   var simplified = simplify(fcLine, 0.001)
   var newLen = simplified.features[0].geometry.coordinates.length
@@ -13,7 +13,7 @@ test('FeatureCollection w/ a LineString in it', function(t) {
 })
 
 test('FeatureCollection w/ a MultiPolygon in it', function(t) {
-  var fsMp = JSON.parse(fs.readFileSync('./geojson/alaska.geojson'))
+  var fsMp = JSON.parse(fs.readFileSync('./test-data/alaska.geojson'))
   var len = JSON.stringify(fsMp, null, '  ').split('\n').length
   var simplified = simplify(fsMp, 0.5)
   var newLen = JSON.stringify(simplified, null, '  ').split('\n').length

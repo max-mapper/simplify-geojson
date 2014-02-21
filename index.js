13a233a5377e1d846d23707e570719966d7a97e8
Max Ogden <max@maxogden.com>
2014-02-21 10:10:58 -0800

    0.0.2 - make simplify function overrideable

--- index.js
+++ index.js
@@ -7,20 +7,24 @@ module.exports = function(geojson, tolerance, dontClone) {
   else return new Error('FeatureCollection or individual Feature required')
 }
 
+module.exports.simplify = function(coordinates, tolerance) {
+  return simplify(coordinates, tolerance)
+}
+
 // modifies in-place
 function simplifyFeature(feat, tolerance) {
   var geom = feat.geometry
   var type = geom.type
   if (type === 'LineString') {
-    geom.coordinates = simplify(geom.coordinates, tolerance)
+    geom.coordinates = module.exports.simplify(geom.coordinates, tolerance)
   } else if (type === 'Polygon' || type === 'MultiLineString') {
     for (var j = 0; j < geom.coordinates.length; j++) {
-      geom.coordinates[j] = simplify(geom.coordinates[j], tolerance)
+      geom.coordinates[j] = module.exports.simplify(geom.coordinates[j], tolerance)
     }
   } else if (type === 'MultiPolygon') {
     for (var k = 0; k < geom.coordinates.length; k++) {
       for (var l = 0; l < geom.coordinates[k].length; l++) {
-        geom.coordinates[k][l] = simplify(geom.coordinates[k][l], 0.5)
+        geom.coordinates[k][l] = module.exports.simplify(geom.coordinates[k][l], tolerance)
       }
     }
   }
